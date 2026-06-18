86b41abd1781caa17669ec4d8b2302088f9f03d1
porbboyza-afk <porbboyza-afk@users.noreply.github.com>
2026-06-18 16:08:41 +0700

    Make training load interpretation explicit

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'mydash-v3-health-20260618-8';
+const CACHE_NAME = 'mydash-v3-health-20260618-9';
 const APP_SHELL = [
   './',
   './index.html',
-  './manifest.json?v=20260618-8',
-  './icon-192.png?v=20260618-8',
-  './icon-512.png?v=20260618-8'
+  './manifest.json?v=20260618-9',
+  './icon-192.png?v=20260618-9',
+  './icon-512.png?v=20260618-9'
 ];
 
 self.addEventListener('install', event => {
