431327a0bdaa5d5a3079ba0c675259547ee8c180
porbboyza-afk <porbboyza-afk@users.noreply.github.com>
2026-06-18 16:15:05 +0700

    Add readable load bands and split wellness views

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'mydash-v3-health-20260618-9';
+const CACHE_NAME = 'mydash-v3-health-20260618-10';
 const APP_SHELL = [
   './',
   './index.html',
-  './manifest.json?v=20260618-9',
-  './icon-192.png?v=20260618-9',
-  './icon-512.png?v=20260618-9'
+  './manifest.json?v=20260618-10',
+  './icon-192.png?v=20260618-10',
+  './icon-512.png?v=20260618-10'
 ];
 
 self.addEventListener('install', event => {
